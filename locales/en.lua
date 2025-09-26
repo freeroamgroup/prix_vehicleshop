05cc72c270fec75c57bdfb407f374f3750abaf2f
Freeroam Company <freeroamcompany@gmail.com>
2025-09-26 14:21:07 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,14 @@
+Locales = Locales or {}
+Locales['en'] = {
+    open_shop = 'Open Vehicle Shop',
+    category = '📂 Categories',
+    buy = '🛒 Buy',
+    cash = '💵 Cash',
+    bank = '🏦 Bank',
+    test_drive = 'Test Drive',
+    time_left = 'Time left: %ss',
+    not_enough = '❌ You do not have enough money.',
+    bought = '✅ You bought %s. Plate: %s',
+    returned = 'Test vehicle returned.',
+    previewing = '👀 Preview: %s - $%s',
+}
